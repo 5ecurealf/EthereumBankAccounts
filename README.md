7d05a6d48706cb1f94a68d97cdf1d0cfa27cc2f7
5ecurealf <alfielloyd12@gmail.com>
2023-07-02 17:23:37 +0100

    inital set up of the projectblueprint of the smart contract creating functional stubs, to layout how the contract will operate

---
+++ README.md
@@ -0,0 +1,26 @@
+# Sample Hardhat Project
+
+This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat help
+npx hardhat test
+REPORT_GAS=true npx hardhat test
+npx hardhat node
+npx hardhat run scripts/deploy.js
+```
+
+Specification:
+
+- Joint bank accounts     
+- deposit 
+- withdrawal
+- users can create accounts with multiple addresses 
+- users can create accounts with multiple owners
+    - each owner has to approve a withdrawal request
+LO's
+- How to use Hardhat
+- Testing 
+- Front end to interact with SC
---
+++ contracts/BankAccount.sol
@@ -0,0 +1,82 @@
+pragma solidity >= 0.4.22 <= 0.8.17;
+// multi account owners
+// anyone can make deposits
+// anyone can request a withdrawal
+// all owners of account must approve the request 
+contract BankAccount {
+    
+    event Deposit(
+        address indexed user,
+        uint indexed accountId,
+        uint value,
+        uint timestamp
+    );
+    event WithdrawRequest(address indexed user,
+        uint indexed accountId,
+        uint indexed withdrawId,
+        uint amount,
+        uint timestamp
+    );
+
+    event Withdraw(uint indexed withdrawId, uint timestamp);
+
+    event AccountCreated(address[] owners, uint indexed id, uint timestamp);
+
+    struct WithdrawRequest {
+        address user;
+        uint amount;
+        uint approvals;
+        mapping(address=>bool) ownersApproved;
+        bool approved;
+    }
+    struct Account {
+        addressp[] owners;
+        uint balance;
+        mapping(uint => WithdrawRequest) withdrawRequests;
+    }
+
+    mapping(uint => Account) accounts;
+    mapping(address => uint[]) userAccounts;
+
+    // variables to create the id's for the accounts 
+    // unique so each time we make a new account we increment the values
+    uint nextAccountId;
+    uint nextWithdrawId;
+
+    function deposit(uint accountId) external payable{
+
+    }
+    // caller will specify otherOwners when creating the account 
+    function createAccount(address[] calldata otherOwners) external {
+
+    }
+
+    function requestWithdrawl(uint accountId, uint amount) external{
+
+    }
+
+    function approveWtihdrawal(uint accountId, uint withdrawId) external{
+
+    }
+
+    function withdraw(uint accountId, uint withdrawId) external {
+        
+    }
+    
+    // getter functions for contract ease of use outside of the contract
+    function getBalance(ui accountId) public view returns (uint) {
+        
+    }
+
+    function getOwners(uint accountId) public view returns (address[] memory) {
+        
+    }
+
+    function getApprovals(uint accountId,uint withdrawId) public view returns (uint) {
+        
+    }
+
+    function getAccounts() public view returns (uint[] memory) {
+        
+    }
+}
